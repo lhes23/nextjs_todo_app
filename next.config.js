78efe301680ed3ad636c8d47a956c61b93086ce0
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-06-05 14:34:41 +0800

    manually set baseurl to getserversideprops

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://nextjs-todo-app-lhes23.vercel.app";
 module.exports = {
-  // nextConfig,
-  basePath: "https://nextjs-todo-app-lhes23.vercel.app",
+  nextConfig,
+  basePath: url,
 };
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 import AddTodoForm from "../components/AddTodoForm";
 import TodosList from "../components/TodosList";
+import { basePath } from "../next.config";
 
 import styles from "../styles/Home.module.css";
 
 export async function getServerSideProps() {
-  const res = await fetch("/api/todo");
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://nextjs-todo-app-lhes23.vercel.app";
+  const res = await fetch(`${baseUrl}/api/todo`);
   const data = await res.json();
   return {
     props: {
@@ -27,6 +33,7 @@ export default function Home({ todos }) {
       <div className={`row ${styles.main}`}>
         <div className="d-flex justify-content-center">
           <h1>NextJS Todo App!</h1>
+          <Link href="/api/todo">TODO</Link>
         </div>
         <div className="col-md col-lg-6">
           <AddTodoForm refreshData={refreshData} />
